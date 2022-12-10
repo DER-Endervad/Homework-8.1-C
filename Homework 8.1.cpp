d874aa9b8ba4a132ce3a1f4755ce3db5518c867b
Даниил <derendervad@gmail.com>
2022-12-10 11:51:13 +0300

    change

--- Homework 8.1.cpp
+++ Homework 8.1.cpp
@@ -8,25 +8,25 @@ int main()
     setlocale(LC_ALL, "Russian");
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    int forbidden_length = 0; std::string str = " ";
+    int forbidden_length = 0, length = 0; std::string str = " ";
+    std::cout << "Введите запретную длину: ";
+    std::cin >> forbidden_length;
     try {
         while (true) {
-            std::cout << "Введите запретную длину: ";
-            std::cin >> forbidden_length;
             std::cout << "Введите слово: ";
             std::cin >> str;
-            forbidden_length = function(str, forbidden_length);
-            std::cout << "Длина слова '" << str << "' равна " << forbidden_length << std::endl;
+            length = function(str, forbidden_length);
+            std::cout << "Длина слова '" << str << "' равна " << length << std::endl;
         }
     }
-    catch (int i) {
-        std::cerr << "Вы ввели слово запретной длины! До свидания" << std::endl;
+    catch (const char* с) {
+        std::cerr << с << std::endl;
     }
 }
 
 int function(std::string str, int forbidden_length) {
     if (str.length() == forbidden_length) {
-        throw -1;
+        throw "Вы ввели слово запретной длины! До свидания";
     }
     else {
         return str.length();
